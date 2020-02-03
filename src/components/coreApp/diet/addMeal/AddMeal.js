e1b37309d2d1c53e923a6fd27e79db06588549cb
KonradWrbl <160815@stud.prz.edu.pl>
2020-02-03 19:34:26 +0100

    fetching data

--- src/components/coreApp/diet/addMeal/AddMeal.js
+++ src/components/coreApp/diet/addMeal/AddMeal.js
@@ -2,12 +2,37 @@ import React, { useState, useEffect } from 'react';
 import { AddMealWrapper, AddMealPopup, BG, FormWrapper, StyledLabel, StyledInput, SubmitButton, SubmitButtonWrapper } from './style';
 import { addMealPopup } from '../../../../redux/actions/actionCreators'
 import { connect } from 'react-redux';
+import Axios from 'axios';
 
 const AddMeal = (props) => {
-    const KEY = 'da38efe5cf3f25794cd0b75eac4a323a'
-    const APPID = '721812f4'
-    const URL = `https://api.edamam.com/api/food-database/parser?ingr=pizza&app_id=${APPID}&app_key=${KEY}`
+    const [newMeal, setNewMeal] = useState('')
+    const [weight, setWeight] = useState(0)
+    const [mealData, setMealData] = useState({})
 
+    const API = `https://api.edamam.com/api/food-database/`
+
+    const fetchData = () => {
+        if(newMeal !== 0) {
+                Axios.get(`${API}/parser?ingr=${newMeal}&app_id=721812f4&app_key=da38efe5cf3f25794cd0b75eac4a323a`)
+            .then(response => {
+                const data = response.data.hints[0].food
+                console.log(response.data.hints[0].food)
+                setMealData({
+                    label: newMeal,
+                    id: data.foodId,
+                    kcal: data.nutrients.ENERC_KCAL,
+                    fat: data.nutrients.FAT,
+                })
+
+            })
+            .catch(error => console.log(error))
+            .finally(response => {
+                props.hidePopup();
+            })
+        } else {
+            console.log('Brak danych');
+        }
+    }
 
     return(
         <>
@@ -16,14 +41,20 @@ const AddMeal = (props) => {
                     <FormWrapper>
                         <StyledLabel>
                             Dish name:
-                            <StyledInput type='text'></StyledInput>
+                            <StyledInput
+                                type='text'
+                                onChange={e => setNewMeal(e.target.value)}
+                            ></StyledInput>
                         </StyledLabel>
                         <StyledLabel>
                             Estimated weight (g):
-                            <StyledInput type='number'></StyledInput>
+                            <StyledInput
+                                type='number'
+                                onChange={e => setWeight(e.target.value)}
+                            ></StyledInput>
                         </StyledLabel>
                         <SubmitButtonWrapper>
-                            <SubmitButton>Add</SubmitButton>
+                            <SubmitButton onClick={fetchData}>Add</SubmitButton>
                         </SubmitButtonWrapper>
                     </FormWrapper>
                 </AddMealPopup>
